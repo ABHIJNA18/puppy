8f56991b363ec17b1817855507c40d1502018fa8
ABHIJNA18 <aabhijnak@gmail.com>
2025-03-12 07:54:16 +0530

    second

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ABHIJNA18/puppy
+
+go 1.23.1
---
+++ puppy.go
@@ -0,0 +1,12 @@
+package puppy
+
+
+func bark() string{
+	return "woof!!"
+}
+
+func barkLoud(y string) string{
+	y="meow!"
+	return y
+}
+
